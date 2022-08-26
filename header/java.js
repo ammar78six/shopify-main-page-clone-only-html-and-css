ec6374a93c548c6ff30376b462f0792d5ff09bc7
Muhammad Ammar <ammartoonice@gmail.com>
2022-08-26 10:15:31 +0500

    Add files via upload

---
+++ header/java.js
@@ -0,0 +1,86 @@
+
+console.log('do')
+let val = true;
+let x = document.getElementById("bfh1");
+
+function myFunction() {
+  if (val== true) {
+    x.style.display = "block";
+    console.log('yes')
+    val=false;
+  } else {
+    x.style.display = "none";
+    console.log('no');
+    val = true;
+  }
+}
+//for second block
+console.log('do2')
+let valu = true;
+let x1 = document.getElementById("bfh2");
+
+function myFunction2() {
+  if (valu== true) {
+    x1.style.display = "block";
+    console.log('yes')
+    valu=false;
+  } else {
+    x1.style.display = "none";
+    console.log('no');
+    valu = true;
+  }
+}
+//for 3
+console.log('do3')
+let value = true;
+let x2 = document.getElementById("bfh3");
+
+function myFunction3() {
+  if (value== true) {
+    x2.style.display = "block";
+    console.log('yes')
+    value=false;
+  } else {
+    x2.style.display = "none";
+    console.log('no');
+    value = true;
+  }
+}
+//for 4
+console.log('do4')
+let va = true;
+let x3 = document.getElementById("bfh4");
+
+function myFunction4() {
+  if (va== true) {
+    x3.style.display = "block";
+    console.log('yes')
+    va=false;
+  } else {
+    x3.style.display = "none";
+    console.log('no');
+    va = true;
+  }
+}
+//for 5
+console.log('do5')
+let learn = true;
+let x4 = document.getElementById("bfh5");
+
+function myFunction5() {
+  if (learn== true) {
+    x4.style.display = "block";
+    console.log('yes')
+    learn=false;
+  } else {
+    x4.style.display = "none";
+    console.log('no');
+    learn = true;
+  }
+}
+
+
+
+
+
+
